f4ccc08c1a92f6c75d5e2648b836ff42401acef2
bayuadji <bayuadji@gmail.com>
2018-03-23 21:10:18 +0700

    add logging and change docker-compose.yml

--- djangodocker/settings.py
+++ djangodocker/settings.py
@@ -123,3 +123,22 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "statics")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+                    'console': {
+                                    'class': 'logging.StreamHandler'
+                                },
+                },
+        'loggers': {
+                    '': {  # 'catch all' loggers by referencing it with the empty string
+                                    'handlers': ['console'],
+                                    'level': 'DEBUG',
+                                },
+                },
+    }
+
+
+
